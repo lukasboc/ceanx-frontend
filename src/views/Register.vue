d937add9c64e8a90f7184a4935a00c8e22efb0e6
Lukas Bock <lukas.bock@uni-oldenburg.de>
2023-02-05 18:06:32 +0100

    margin-top on register info

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -78,7 +78,7 @@
     <div class="card-actions justify-center mt-4">
       <button v-if="step === 0" class="btn btn-primary" v-bind:disabled="this.disableRegisterButton" type="submit" >Registrieren</button>
     </div>
-        <div v-if="!registerAllowed" class="alert alert-info shadow-lg">
+        <div v-if="!registerAllowed" class="alert alert-info shadow-lg mt-4">
           <div>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-current flex-shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
             <span>Die Registrierungsfunktion wurde deaktiviert. Bitte gib einem Nutzer Bescheid, die Funktionalität vorübergehend zu aktivieren.</span>
