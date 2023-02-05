d5ea5d18997fa8799e6a7ccc311b0dd3f9f68010
Lukas Bock <lukas.bock@uni-oldenburg.de>
2023-02-05 17:45:57 +0100

    fix input type password

--- src/components/NewJiraConnection.vue
+++ src/components/NewJiraConnection.vue
@@ -50,7 +50,7 @@
           <label class="label" for="api_token">
             <span class="label-text">API Token <span class="badge badge-info badge-outline"><a href="https://support.atlassian.com/atlassian-account/docs/manage-api-tokens-for-your-atlassian-account/" target="_blank">How to?</a></span></span>
           </label>
-          <input v-model="api_token" type="text" id="api_token" placeholder="29z2dxePUAHZo3oGG92H97NA" class="input input-bordered input-primary w-xl">
+          <input v-model="api_token" type="password" id="api_token" placeholder="29z2dxePUAHZo3oGG92H97NA" class="input input-bordered input-primary w-xl">
           <div class="alert alert-warning shadow-lg mt-2" v-for="error of v$.api_token.$errors" :key="error.$uid">
             <div>
               <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg>
