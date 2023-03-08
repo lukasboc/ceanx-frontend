b4e64a8b6a5a8627006d9c30807a3aa230fbac39
Lukas Bock <lukas.bock@uni-oldenburg.de>
2023-03-08 16:42:44 +0100

    fix typo

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -14,7 +14,7 @@
           <div class="alert alert-info shadow-lg">
             <div>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-current flex-shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-              <span>Hier findest du alle genutzten Jira und Troi Verbindungen, die zur Unterstützung für Aufwandschätzungen genutzt werden können. Diese Verbindungen werden global genutzt. Mehrfache Einträge für z.B. eine Jira Instanz sind also <strong>nicht nötig</strong>. Das Löschen einer Verbindung geschieht ebenfalls global..</span>
+              <span>Hier findest du alle genutzten Jira und Troi Verbindungen, die zur Unterstützung für Aufwandschätzungen genutzt werden können. Diese Verbindungen werden global genutzt. Mehrfache Einträge für z.B. eine Jira Instanz sind also <strong>nicht nötig</strong>. Das Löschen einer Verbindung geschieht ebenfalls global.</span>
             </div>
           </div>
           <p></p>
